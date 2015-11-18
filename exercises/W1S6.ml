133792f445433bc9046fc8b4e4791c8e062c154c
Antonios Klimis <antonios.klimis@gmail.com>
2015-11-18 15:33:45 +0200

    Formating changes

--- exercises/W1S6.ml
+++ exercises/W1S6.ml
@@ -9,17 +9,15 @@ let rec gcd n m =
 let is_prime n =
   let n = abs n in
   let rec is_not_divisor d =
-    d * d > n || 
-    (n mod d <> 0 && is_not_divisor (d+1)) in
-  n <> 1 && is_not_divisor 2;;
+    d * d > n || (n mod d <> 0 && is_not_divisor (d+1)) 
+  in n <> 1 && is_not_divisor 2;;
 
 
 let multiple_upto n r =
   let n = abs n in		
 		let rec is_divisor d = 
     if (n mod d) = 0 
-    then 
-      true 
+    then true 
     else 
-      (d < r && is_divisor (d+1)) in
-		r <> 1 && is_divisor 2;;
+      (d < r && is_divisor (d+1)) 
+    in r <> 1 && is_divisor 2;;
