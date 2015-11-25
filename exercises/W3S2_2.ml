cfa8d8918ef54709b7d758bd23b5aaf2d13abb7f
Antonios Klimis <antonios.klimis@gmail.com>
2015-11-25 16:15:11 +0200

    Change implementation to a faster one

--- exercises/W3S2_2.ml
+++ exercises/W3S2_2.ml
@@ -3,16 +3,15 @@ let rec mem x l =
   | [] -> false
   | x' :: xs ->	(x' = x || mem x xs) ;;
 
-(* TODO: faster version. *)
-let rec append l1 l2 = l1 @ l2;;
+let rec append l1 l2 = 
+	match l1 with
+  | [] -> l2
+  | hd :: tl -> hd :: (append tl l2);;
 
-(* TODO: faster version. *)
 let rec combine l1 l2 =
-	let rec combine_helper l l1 l2 =
 	  match (l1, l2) with
-	  | ([],[]) -> l
-	  | (x::xs,x'::xs') -> combine_helper (l @ [(x,x')]) xs xs' 
-	in combine_helper [] l1 l2;;
+	  | ([],[]) -> []
+	  | (x::xs,x'::xs') -> (x,x') :: (combine xs xs');;
 
 let rec assoc l k =
   match l with
