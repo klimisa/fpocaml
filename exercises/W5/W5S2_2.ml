bc9ff41b98bb706ccfcc357925de8cf459f5013b
Antonios Klimis <antonios.klimis@gmail.com>
2015-12-12 23:08:38 +0200

    w5s2_2 completed

--- exercises/W5/W5S2_2.ml
+++ exercises/W5/W5S2_2.ml
@@ -10,41 +10,113 @@ let rec print_path path =
    | [] -> print_bytes ""
    | [x] -> print_bytes x 
    | x::xs -> 
-   		(print_bytes (x^"/")); 
-   		print_path xs;;
+   		(print_bytes (x^"/"); 
+   		print_path xs);;
 
 let rec print_file lvl name =
 	if lvl = 0 then 
 		print_bytes name
 	else
-		let _ = print_bytes "|" in
-		print_file (lvl - 1) name;;
+		(print_bytes "|";
+		print_file (lvl - 1) name);;
 
 let rec print_symlink lvl name path =
-  	(print_file lvl name);
-  	(print_bytes "->"); 
-  	(print_path path);
-  	 ;;
+  	(print_file lvl name;
+  	 print_bytes "->"; 
+  	 print_path path);;
 
 let rec print_dir lvl name =
-  "Replace this string with your implementation." ;;
+	if lvl = 0 then
+		(print_bytes "/"; 
+		print_bytes name)
+	else
+		(print_bytes "|";
+		print_dir (lvl - 1) name);;
 
 let print_filesystem root =
-  (* This pre-completed structure is only here to help you.
-     If it confuses you, don't hesitate to change it. *)
   let rec print_filesystem lvl items =
-    "Replace this string with your implementation." in
-  print_filesystem 0 root ;;
+    match items with
+    | [] -> print_bytes ""
+    | (name,node)::ns -> 
+        match node with 
+        | File ->
+            begin
+              print_file lvl name; 
+              print_newline();
+              print_filesystem lvl ns
+            end
+        | Dir filesystem ->
+            begin      
+              print_dir lvl name; 
+              print_newline(); 
+              print_filesystem (lvl+1) filesystem;
+              print_filesystem lvl ns
+            end
+        | Symlink path -> 
+            begin       
+              print_symlink lvl name path; 
+              print_newline();
+              print_filesystem lvl ns
+            end
+  in print_filesystem 0 root ;; 
 
-let rec resolve sym path =
-  (* This pre-completed structure is only here to help you.
-     If it confuses you, don't hesitate to change it. *)
-  let rec resolve acc path =
-    "Replace this string with your implementation."  in
-  resolve (List.tl (List.rev sym)) path ;;
+let rec resolve sym path = 
+  let rec resolves acc path =
+    match path with
+    | [] -> List.rev acc
+    | x::xs -> 
+        if x = ".." then
+          if List.length acc >= 1 then
+            resolves (List.tl acc) xs
+          else
+            resolves acc xs
+        else 
+          resolves ([x]@acc) xs
+  in resolves (List.tl (List.rev sym)) path ;;
+  
+  let rec file_exists root path =
+  match path with
+  | [] -> false
+  | p::ps ->
+  	  match root with
+	  | [] -> false
+	  | (name,node)::ns ->
+  	      if name = p then 
+            match node with
+            | File -> true
+            | Dir filesystem -> file_exists filesystem ps
+            | Symlink path ->  true
+          else file_exists ns path;;
 
-let rec file_exists root path =
-  "Replace this string with your implementation." ;;
-
-(* move print_filesystem here for exercise 8 *)
+let print_filesystem root =
+  let rec print_filesystem lvl items sym =
+    match items with
+    | [] -> print_bytes ""
+    | (name,node)::ns -> 
+        match node with
+        | File ->
+            begin
+              print_file lvl name; 
+              print_newline();
+              print_filesystem lvl ns sym
+            end
+        | Dir filesystem ->
+            begin
+              print_dir lvl name; 
+              print_newline(); 
+              print_filesystem (lvl+1) filesystem (sym@[name]);
+              print_filesystem lvl ns sym;
+            end
+        | Symlink path -> 
+        	let valid_path =
+          	if file_exists root (resolve (sym@[name]) path) then
+          		path
+          	else 
+          		["INVALID"] in
+          	  begin
+			          print_symlink lvl name valid_path; 
+			          print_newline();
+			          print_filesystem lvl ns sym
+	            end
+  in print_filesystem 0 root [];; 
 
