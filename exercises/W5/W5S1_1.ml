5e904aaac8f2dbf18d1c71ce0f17f266dddeca2c
Antonios Klimis <antonios.klimis@gmail.com>
2015-12-14 10:07:22 +0200

    deleted unused stuff

--- exercises/W5/W5S1_1.ml
+++ exercises/W5/W5S1_1.ml
@@ -38,19 +38,6 @@ let bal_height bst =
         with	
         | Unbalanced a -> (0, a+1) 
   in height 0 bst;;
-
-bal_height
-  (Node 
-  		(Node 
-  			(Node 
-  				(Empty, -2, Empty), 1, Node (Empty, -1, Empty)), 
-  	-1,
-    Node 
-    	(Node 
-    		(Empty, 0, Empty), 
-    		0, 
-    	Node 
-    		(Empty, -3, Empty))))
     
 let balanced_fast bst =
   "Replace this string with your implementation." ;;
